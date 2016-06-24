58975c1a39425c57738d27effb4292546e2fe3b5
ezpraktika <gfhjkmubn[f,1>
2016-06-25 00:05:52 +0400

    new line

--- src/practice/Main.java
+++ src/practice/Main.java
@@ -5,5 +5,6 @@ public class Main {
         System.out.println("hello");
         System.out.println("eyey");
         System.out.println("SEREJA MLG");
+        System.out.println("SEREJA MLG tochno");
     }
 }
