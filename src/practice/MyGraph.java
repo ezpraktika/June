f86f96465c10bcb0ddea75822eae87e38e986ba9
ezpraktika <gfhjkmubn[f,1>
2016-06-25 11:19:59 +0400

    updated mygraph class

--- src/practice/MyGraph.java
+++ src/practice/MyGraph.java
@@ -6,26 +6,28 @@ import java.util.ArrayList;
 
 public class MyGraph {
     private  boolean isVisited[];                     //массив посещенных
+    private ArrayList<Integer> history;             //порядок посещения
     private ArrayList<ArrayList<Integer>> adjLists; //список смежности
 
     public MyGraph(int n) {
         initGraph(n);
     }
 
-    private void dfs_rec( int v){
+    public void dfs( int v){
         isVisited[v] = true;
+        history.add(v);
         System.out.print(v + " ");
         for(int w : adjLists.get(v)){
             if(!isVisited[w]){
-                dfs_rec(w);
+                dfs(w);
             }
         }
     }
 
     public void initGraph(int n) {
         isVisited = new boolean[n];
-
-        ArrayList<ArrayList<Integer>> adjLists = new ArrayList<ArrayList<Integer>>();
+        history = new ArrayList<Integer>(n);
+        adjLists = new ArrayList<ArrayList<Integer>>();
 
         for(int v=0; v<n; v++){
             adjLists.add(new ArrayList<Integer>());
@@ -35,5 +37,7 @@ public class MyGraph {
     public void createEdge(int from, int to) {
         adjLists.get(from).add(to);
     }
+
+
 }
 
