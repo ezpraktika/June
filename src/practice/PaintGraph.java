549beff7bcd50fd073781a4eb571bb2ead3dfd37
n <qwertypalmO@gmail.com>
2016-06-25 11:20:32 +0300

    HAHA

---
+++ src/practice/PaintGraph.java
@@ -0,0 +1,26 @@
+package practice;
+
+import javax.swing.*;
+import java.util.ArrayList;
+
+/**
+ * Created by n on 25.06.2016.
+ */
+public class PaintGraph extends JPanel{
+    public PaintGraph(ArrayList<ArrayList<Integer>> adjacencyList) {
+
+        mxGraph graph = new mxGraph();
+        Object parent = graph.getDefaultParent();
+
+        int n = adjacencyList.size();
+        Object points[] = new Object[n];
+
+        double phi0 = 0;
+        double phi = 2 * Math.PI / n;
+        int r = 300;
+        for (int i = 0; i < points.length; i++) {
+            points[i] = graph.insertVertex(parent, null, i, 400 + r * Math.cos(phi0), 400 + r * Math.sin(phi0), 20, 20);
+            phi0 += phi;
+        }
+    }
+}
